47b81341f42c47679b24734280d68f5d8afb3be9
Vadzim_Yesman <vvyesman@gmail.com>
2022-01-05 15:21:09 -0800

    Finished the intermediate part

--- index.js
+++ index.js
@@ -1,27 +1,19 @@
 console.log("Hello world!")
 
+
 let message = document.querySelector("#message")
+console.log(message)
 
-
-const crossOffMovie = (event) =>{
-
-    event.target.classList.toggle("checked")
-
-    if (event.target.classList.contains("checked")){
-        message.textContent="Movie watched!"
-    } else {
-        message.textContent="Movie added back!"
-    } 
-
-}
 
 
 const addMovie = (event) =>{
     event.preventDefault()
 
     let inputField=document.querySelector("input")
+    console.log(inputField)
 
     let movie=document.createElement("li")
+    console.log
      
     let movieTitle=document.createElement("span")
 
@@ -52,13 +44,47 @@ const addMovie = (event) =>{
 
 
 
-
 const deleteMovie = (event) =>{
     event.target.parentNode.remove()
-    message.textContent="Movie deleted!"
+    
+    message.textContent=`${event.target.previousSibling.textContent} deleted`
+    revealMessage()
+    console.log(event)
 }
 
+
 document.querySelector("form").addEventListener("submit", addMovie)
+
+const crossOffMovie = (event) =>{
+
+    event.target.classList.toggle("checked")
+
+    let MOVIE = event.target.textContent
+
+    if (event.target.classList.contains("checked")){
+        
+        message.textContent=`${MOVIE} watched!`
+
+    } else {
+        message.textContent= `${MOVIE} added back!`
+    } 
+
+    revealMessage()
+
+}
+
+const revealMessage = () =>{
+
+    message.classList.remove("hide")
+
+
+    setTimeout(()=>{
+        message.classList.add("hide")
+    },1000)
+}
+
+
+
 
 
 
